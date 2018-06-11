d2711b1ff5e1db16d642dd7ad602cdaf99d53bb1
James <cal12011@byui.edu>
2018-06-11 12:19:33 -0600

    Added boolean check

--- feature/src/main/java/cs246/priorengagement/feature/MainActivity.java
+++ feature/src/main/java/cs246/priorengagement/feature/MainActivity.java
@@ -5,9 +5,12 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private boolean James;
+    //change 1 James
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
