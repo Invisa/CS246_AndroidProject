58ea535eabd69d958ddf1cb9745506d08982ba4b
Invisa <enriquekolowich@gmail.com>
2018-06-20 12:09:04 -0600

    EK12:08

--- app/src/main/java/com/timecrunch/timecrunch/Event.java
+++ app/src/main/java/com/timecrunch/timecrunch/Event.java
@@ -3,10 +3,13 @@ package com.timecrunch.timecrunch;
 public class Event {
 
     // Event
-    Boolean eventLocal;
     String title;
     String note;
+    Boolean gDatabase; // local is 0; external/google is 1;
 
+    // Location
+    String location;
+    Boolean gMaps; // is formatted for Google Maps
     // time
     String defaultEventStart;
     Boolean timeAMPM;
@@ -26,6 +29,38 @@ public class Event {
     Boolean saturday;
     Boolean sunday;
 
+
+    // Default Constructor
+    public Event() {
+        title = NULL;
+        location = NULL;
+        gMaps = NULL;
+        gDatabase = NULL;
+
+
+        defaultEventStart = NULL;
+        timeAMPM = NULL;
+        timeStart = NULL;
+        timeEnd = NULL;
+        timeHour = NULL;
+        timeMinute = NULL;
+
+
+        repeat = NULL;
+        daily = NULL;
+        weekly = NULL;
+        other = NULL;
+        monday = NULL;
+        tuesday = NULL;
+        wednesday = NULL;
+        thursday = NULL;
+        friday = NULL;
+        saturday = NULL;
+        sunday = NULL;
+
+        note = NULL;
+    }
+
     public void setTitle(String title) {
         this.title = title;
     }
@@ -34,7 +69,8 @@ public class Event {
         // Parse & JSON/GSON
     }
 
-    //public void () setDefaultTimeStart() {
+    /*
+    public void () setDefaultTimeStart() {
         // Parse in string from google calander day events
         //find first available  empty time Set timeStart to next hour (ex: if its 7:55, set to 8:00)
         //return empty time to defaultTimeStart
@@ -42,7 +78,8 @@ public class Event {
         //rules 1 must be empty in calender
         //rules 2 must be present or future
 
-    //}
+    }
+    */
 
     public void timeCrunch() {
 
